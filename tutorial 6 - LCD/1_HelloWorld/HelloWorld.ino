232de67e9fbd20607b6b229b7b8910a46de10b7b
Peter Pan <twinkle-pirate@hotmail.com>
2018-11-01 00:45:09 +0800

    fix tutorial name, fix grove chinese doc

---
+++ tutorial 6 - LCD/1_HelloWorld/HelloWorld.ino
@@ -0,0 +1,25 @@
+//include the rgb_lcd library
+#include "rgb_lcd.h"
+
+//assign name lcd to rgb_lcd
+rgb_lcd lcd;
+
+void setup() 
+{
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    
+    // Print Hello, World! to the LCD.
+    lcd.print("Hello, World!");
+    delay(1000);
+}
+
+void loop() 
+{
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(millis()/1000);
+    delay(100);
+}
